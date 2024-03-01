2a45eee52733c43a116a4df123df3a4ae930341c
michal <michalmaselko@wp.pl>
2024-03-01 11:09:37 +0100

    add xml file

--- src/main/java/org/cncTools/App.java
+++ src/main/java/org/cncTools/App.java
@@ -1,13 +1,23 @@
 package org.cncTools;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+public class App {
+    public static void main( String[] args ) {
+        System.setProperty("hadoop.home.dir", "C:\\winutil\\");
+        SparkSession sparkSession = SparkSession.builder()
+                .appName("AppName")
+                .master("local")
+                .getOrCreate();
+
+        Dataset<Row> df = sparkSession.read()
+                .format("xml")
+                .option("rowTag", "cd")
+                .load("src/main/resources/cd_catalog.xml");
+
+        df.show();
+        df.printSchema();
     }
 }
