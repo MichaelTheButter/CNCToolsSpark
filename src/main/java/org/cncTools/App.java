1d947a2a1f980a903a054a731c20f65005eaeae6
michal <michalmaselko@wp.pl>
2024-03-04 18:58:27 +0100

    add data files and loaders

--- src/main/java/org/cncTools/App.java
+++ src/main/java/org/cncTools/App.java
@@ -3,21 +3,30 @@ package org.cncTools;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.ml.source.image.*;
+import org.cncTools.loaders.LoadBitsBitsTools;
+import org.cncTools.loaders.LoadSandvikTools;
+
+import static org.apache.spark.sql.functions.*;
 
 public class App {
     public static void main( String[] args ) {
-        System.setProperty("hadoop.home.dir", "C:\\winutil\\");
         SparkSession sparkSession = SparkSession.builder()
                 .appName("AppName")
-                .master("local")
+                .master("local[*]")
                 .getOrCreate();
 
-        Dataset<Row> df = sparkSession.read()
-                .format("xml")
-                .option("rowTag", "cd")
-                .load("src/main/resources/cd_catalog.xml");
-
+        LoadSandvikTools loader = new LoadSandvikTools(sparkSession);
+        Dataset<Row> df = loader.loadJoinedSandvikCatalog();
         df.show();
         df.printSchema();
+
+        LoadBitsBitsTools bbloader = new LoadBitsBitsTools(sparkSession);
+        Dataset<Row> df2 = bbloader.loadBitsBits();
+
+        df2.show();
+        df2.printSchema();
+
+
     }
 }
